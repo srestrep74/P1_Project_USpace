b5b98966a09ed3355ce45c2565b4b4bda85fdeeb
JuanM0412 <juangomezp1204@gmail.com>
2023-08-07 11:06:33 -0500

    - Added database connection - Code for connection from Arduino to Ubidots

--- USpace/settings.py
+++ USpace/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'USpace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'birzyay0v1nhjnvywaun',  # Replace with your MariaDB database name
+        'USER': 'ud03785zpvgrc7vl',  # Replace with your MariaDB database username
+        'PASSWORD': ,  # Replace with your MariaDB database password
+        'HOST': 'birzyay0v1nhjnvywaun-mysql.services.clever-cloud.com',  # Replace with your MariaDB database host
+        'PORT': 3306,  # Replace with your MariaDB database port (usually 3306)
     }
 }
 
