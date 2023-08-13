58db10e0686f6b56e25f009aa192d4b39c1a0144
JuanM0412 <juangomezp1204@gmail.com>
2023-08-13 05:54:01 -0500

    Workshop 1 done Data were added inside the admin models (Spaces entity), as well I created a view to see that data.

--- admins/models.py
+++ admins/models.py
@@ -2,11 +2,22 @@ from django.db import models
 
 
 class Space(models.Model):
+    AVAILABILITY = (
+        (0, 'Disponible'),
+        (1, 'Ocupado'),
+        (2, 'Deshabilitado')
+    )
+    CLASIFICATION = (
+        (0, 'Escenario deportivo'),
+        (1, 'Mesa de restaurante'),
+        (2, 'Zona de descanso')
+    )
+
     id = models.AutoField(primary_key=True, editable=False, unique=True)
     name = models.CharField(max_length=150, blank=False, null=False)
-    description = models.CharField(max_length=500, blank=False, null=False)
-    availability = models.IntegerField(blank=False, null=False)
-    clasification = models.IntegerField(blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    availability = models.IntegerField(blank=False, null=False, choices=AVAILABILITY)
+    clasification = models.IntegerField(blank=False, null=False, choices=CLASIFICATION)
 
     def __str__(self):
         return self.name
