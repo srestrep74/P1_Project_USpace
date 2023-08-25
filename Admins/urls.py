f7394fd36bb54c4a81cbd196f8790636d0df4073
JuanM0412 <juangomezp1204@gmail.com>
2023-08-25 15:26:27 -0500

    The view for editing spaces is up and running.

--- Admins/urls.py
+++ Admins/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('show_spaces' , views.viewSpaces , name='show_spaces'),
-    path('add_space' , views.addSpace , name='add_space')
+    path('add_space' , views.addSpace , name='add_space'),
+    path('edit_space/<int:id>' , views.editSpace , name='edit_space')
 ]
--- Admins/views.py
+++ Admins/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import *
 
 
@@ -24,3 +25,21 @@ def addSpace(request):
         form = SpaceForm()
     
     return render(request, 'add_space.html', {'form': form})
+
+
+def editSpace(request, id):
+    space_form, error = None, None
+    try:
+        space = Space.objects.get(id=id)
+        if request.method == 'GET':
+            space_form = SpaceForm(instance=space)
+        else:
+            space_form = SpaceForm(request.POST, instance=space)
+            if space_form.is_valid():
+                space_form.save()
+                return redirect('show_spaces')
+
+    except ObjectDoesNotExist as e:
+        error = f'No se ha encontrado un espacio con el ID {id}.'
+
+    return render(request, 'edit_space.html', {'space_form': space_form, 'error': error})
