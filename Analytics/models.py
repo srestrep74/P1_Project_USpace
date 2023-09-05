d258bebd0358135f2799b58143c87cdb683cfe68
JuanM0412 <juangomezp1204@gmail.com>
2023-09-05 13:06:59 -0500

    Minor change

--- Analytics/models.py
+++ Analytics/models.py
@@ -1,23 +1,15 @@
 from django.db import models
 from Spaces.models import *
 from Admins.models import *
-<<<<<<< HEAD
-=======
 
->>>>>>> juan
 
 class OcuppiedSpace(models.Model):
     id = models.AutoField(primary_key=True, editable=False, unique=True)
     space_id = models.ForeignKey(Space, on_delete=models.CASCADE)
-<<<<<<< HEAD
-    occupied_at = models.DateTimeField()
-    unoccupied_at = models.DateTimeField()
-=======
     occupied_at = models.DateTimeField(auto_now_add=True)
     unoccupied_at = models.DateTimeField(auto_now=True)
     longitude = models.FloatField()
     latitude = models.FloatField()
->>>>>>> juan
 
     def __str__(self):
         return str(self.id)
