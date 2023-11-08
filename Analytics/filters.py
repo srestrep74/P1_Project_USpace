f5476f6cbd33294130e9025bc3273faafddeffac
Miguel Hoyos <mahoyosv@eafit.edu.co>
2023-11-08 12:39:06 -0500

    Label traduction from space filter

--- Analytics/filters.py
+++ Analytics/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from .models import OcuppiedSpace
+from .models import OcuppiedSpace, Space
 from django import forms
 from django_filters import DateFromToRangeFilter
 
@@ -7,6 +7,8 @@ from django_filters import DateFromToRangeFilter
 class OccupiedSpaceFilter(django_filters.FilterSet):
     start = django_filters.DateFilter(field_name='occupied_at', lookup_expr='gte', label='Fecha inicial',widget=forms.DateInput(attrs={'type': 'date'}))
     end = django_filters.DateFilter(field_name='occupied_at', lookup_expr='lte', label='Fecha final',widget=forms.DateInput(attrs={'type': 'date'}))
+    space_id = django_filters.ModelChoiceFilter(field_name='space_id', queryset=Space.objects.all(), label='Espacio')
+
 
     class Meta:
         model = OcuppiedSpace
