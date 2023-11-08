632fad140bbb705f3dd9bb956e2453d9a0f33d70
JuanM0412 <juangomezp1204@gmail.com>
2023-11-08 05:59:16 -0500

    Usability

--- Auth/models.py
+++ Auth/models.py
@@ -6,7 +6,7 @@ class user_m(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     username = models.CharField(max_length=150)
     email = models.EmailField(null=True)
-    profile_pic = models.ImageField(default='profile.jpg', null=True)
+    profile_pic = models.ImageField(default='profile.jpg', null=True, upload_to='user/images')
     
     def __str__(self):
         return str(self.username)
--- static/map.js
+++ static/map.js
@@ -6,10 +6,11 @@ const bounds = [
 
 // Crear un mapa centrado en la Universidad EAFIT y con límite de visualización en la zona de EAFIT
 const map = L.map('map', {
-    minZoom : 18, // Ajusta el límite incluso cuando el usuario intente hacer zoom fuera de los límites.
+    maxBounds: bounds,
+    maxBoundsViscosity: 0.7, // Ajusta la viscosidad de los límites
+    minZoom: 15, // Ajusta el nivel de zoom inicial
     dragging: true,
-    zoomControl: false
-}).setView([6.20019, -75.57844], 20);
+}).setView([6.20020, -75.57842], 20); // Ajusta el nivel de zoom
 
 // Agregar la capa base de OpenStreetMap
 //L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}').addTo(map);
