734fbee165218499db812e665194d62b71a4f1a8
JuanM0412 <juangomezp1204@gmail.com>
2023-11-08 15:54:20 -0500

    Map fixed

--- static/map.js
+++ static/map.js
@@ -1,15 +1,16 @@
 //Coordenadas de la esquina superior izquierda y esquina inferio derecha que forman un rectangulo encerrando la zona
 const bounds = [
-    [6.202708, -75.58052], // Esquina superior izquierda
-    [6.197644, -75.5757], // Esquina inferior derecha
+    [6.20358,-75.57915], // Esquina superior izquierda
+    [6.19657,-75.57830], // Esquina inferior derecha
 ];
 
 // Crear un mapa centrado en la Universidad EAFIT y con límite de visualización en la zona de EAFIT
 const map = L.map('map', {
     maxBounds: bounds,
     maxBoundsViscosity: 0.7, // Ajusta la viscosidad de los límites
-    minZoom: 15, // Ajusta el nivel de zoom inicial
+    minZoom: 17, // Ajusta el nivel de zoom inicial
     dragging: true,
+    zoomControl: false,
 }).setView([6.20020, -75.57842], 20); // Ajusta el nivel de zoom
 
 // Agregar la capa base de OpenStreetMap
